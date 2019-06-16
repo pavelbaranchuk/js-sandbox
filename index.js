0be2d13e84e8e061eb3b41ddfab21075f66b25d3
Pavel Baranchuk <pavel@Pavels-MacBook-Pro.local>
2019-06-16 15:04:51 +0300

    initial setup

---
+++ index.js
@@ -0,0 +1,16 @@
+var peoples = [["оксана", 15], ["василий", 21], ["сереженька", 43]]
+var objPeople = []
+function otvet(array){
+	for (var i = 0; i < array.length; i++){
+ var objPerson = {}
+ objPerson.name = array[i][0];
+ objPerson.age = array[i][1];
+ objPeople.push(objPerson)
+ }
+ return objPeople
+}
+
+var check = otvet(peoples);
+console.log("check", check)
+
+document.getElementById("app").innerHTML = JSON.stringify(check);
