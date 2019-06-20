8b0fec53e2fcb63f807c288c8cf406e228c6bd7d
karina-shribaylo <karynamishchenko@gmail.com>
2019-06-20 19:02:10 +0300

    changes

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-var peoples = [["оксана", 15], ["василий", 21], ["сереженька", 43]]
-var objPeople = []
-function otvet(array){
-	for (var i = 0; i < array.length; i++){
- var objPerson = {}
- objPerson.name = array[i][0];
- objPerson.age = array[i][1];
- objPeople.push(objPerson)
- }
- return objPeople
-}
+var arr = [1, 2, 3, 0, 4, 5, 6] 
 
-var check = otvet(peoples);
-console.log("check", check)
+var result = 0;
+arr.reduce(function(res, elem) {
+if (elem === 0) {
+   result = res;
+   return res;
+  }
+ return res + elem;
+})
 
-document.getElementById("app").innerHTML = JSON.stringify(check);
+	document.write(result)
+	
