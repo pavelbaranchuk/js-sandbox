692720e5852aaae64a5af84eea195dc627385894
Denys Skiba <dyosssna@gmail.com>
2019-06-22 17:16:52 +0300

    3 buttons

--- index.js
+++ index.js
@@ -2,13 +2,7 @@ const movies = {
 	'latest': 'https://raw.githubusercontent.com/pavelbaranchuk/js-sandbox/master/API/latest.json',
 	'popular': 'https://raw.githubusercontent.com/pavelbaranchuk/js-sandbox/master/API/popular.json',
 	'upcoming': 'https://raw.githubusercontent.com/pavelbaranchuk/js-sandbox/master/API/upcoming.json'
-
-<<<<<<< HEAD
-var check = otvet(peoples);
-console.log("check", check)
-=======
 }
->>>>>>> origin
 
 async function getMovies(choice) { 
   try {
