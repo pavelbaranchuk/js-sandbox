fe51dd9183956efeca7916a85ddf2655369ee2b5
karina-shribaylo <karynamishchenko@gmail.com>
2019-07-06 14:23:01 +0300

    how to add output by number and genre

--- index.js
+++ index.js
@@ -1,13 +1,75 @@
-var arr = [1, 2, 3, 0, 4, 5, 6] 
+const movies = {
+  'latest': 'https://raw.githubusercontent.com/pavelbaranchuk/js-sandbox/master/API/latest.json',
+  'popular': 'https://raw.githubusercontent.com/pavelbaranchuk/js-sandbox/master/API/popular.json',
+  'upcoming': 'https://raw.githubusercontent.com/pavelbaranchuk/js-sandbox/master/API/upcoming.json'
 
-var result = 0;
-arr.reduce(function(res, elem) {
-if (elem === 0) {
-   result = res;
-   return res;
+}
+
+var moviesArray = [];
+var val;
+
+async function getMovies(choice, num) {
+  try {
+    const response = await fetch(movies[choice]);
+    const moviesArray = await response.json();
+    // console.log("moviesArray", moviesArray)
+    createMovies(moviesArray, num);
+    // delete console.log and uncomment function when it will be implemented
+  } catch (err) {
+    console.log("Fetch Error :-S", err);
+    return null;
   }
- return res + elem;
-})
+}
+var val2
 
-	document.write(result)
-	
+document.querySelector(".button").addEventListener('click', function numMovies() {
+  val = document.querySelector('.input').value;
+  getMovies('latest', val);
+});
+document.querySelector(".button2").addEventListener('click', function numMovies() {
+  val2 = document.querySelector('.input2').value;
+  getMovies('latest');
+});
+
+function createMovies(array, num) {
+  
+  for (var i = 0; i < 20; i++) {
+    
+    var list = document.getElementById('list');
+
+    var divListItem = document.createElement('div');
+    var divPoster = document.createElement('div');
+    var genre = document.createElement('span');
+    var img = document.createElement("img");
+    var rating = document.createElement('span');
+    var title= document.createElement('span');
+
+    if ( val === array[i].genre) {
+      
+    divListItem.classList.add('list__item');
+   
+    divPoster.classList.add('poster');
+    genre.classList.add('genre');
+    rating.classList.add('rating');
+    title.classList.add('title');
+  
+  
+    img.src = array[i].poster
+    genre.innerHTML = array[i].genre
+    rating.innerHTML = array[i].rating
+    title.innerHTML = array[i].title
+  
+  
+    divPoster.appendChild(genre);
+    divPoster.appendChild(img);
+    divPoster.appendChild(rating);
+  
+    divListItem.appendChild(divPoster);
+    divListItem.appendChild(title);
+    
+    list.appendChild(divListItem);
+  }
+
+}
+}
+  getMovies('latest',20)
